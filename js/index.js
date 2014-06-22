3c85e950cb684e8569aafaaee07e6eae4a1265b7
cberkay <c.berkayantmen@gmail.com>
2014-06-22 13:50:15 -0400

    more info

--- js/index.js
+++ js/index.js
@@ -15,8 +15,7 @@ function keep_track(number) {
 		Web Crawler Projects <br>\
 			- Information of Toronto residents, based on their streets, transferred the information from <br>\
 			411.ca into an excel spreadsheet <br>\
-			- A simple web app that asks you for a professor and his course code and then gives you comments <br>\ 
-				about him from ratemyprofessors.com <br>\
+			- A simple web app that asks you for a professor and his course code and then gives you comments <br> about him from ratemyprofessors.com <br>\
 			- Stored all the of asos.com products names, prices and urls in a database"
 		);
 	}
